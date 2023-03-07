3aa3e07081936a7d214e5a90c58adae9329dd3fc
Dahian Nuñez <dayannunez.ba@gmail.com>
2023-03-07 03:11:53 -0600

    Panel de control validador de perfiles

--- capaPresentacionCliente/App_Start/BundleConfig.cs
+++ capaPresentacionCliente/App_Start/BundleConfig.cs
@@ -12,23 +12,25 @@ namespace capaPresentacionCliente
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new Bundle("~/bundles/complementos").Include(
-                         "~/Scripts/Index.js"));
+                       "~/Scripts/fontawesome/all.min.js",
+                       "~/Scripts/scripts.js",
+                       "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.responsive.js",
+                         "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                         "~/Scripts/sweetalert.min.js"
+                        ));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
 
-            //// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
-            //// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/Index.css",
-                      "~/Content/site.css"));
+                "~/Content/site.css",
+                "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css"
+                ));
         }
     }
 }
